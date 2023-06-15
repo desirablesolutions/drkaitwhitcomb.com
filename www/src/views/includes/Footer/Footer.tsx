09088c1fe4396c7b36061dd57ed51dfd3b1b2ec5
Jeffrey Desir <jeffrey@desir.tech>
2023-06-15 18:28:45 -0400

    [ mostly working ]
    adding blakprint considerations next and layout data.

---
+++ www/src/views/includes/Footer/Footer.tsx
@@ -0,0 +1,36 @@
+export default function Footer() {
+    return (
+        <footer className="w-full p-4 border-t border-gray-200 shadow relative-20 md:flex md:items-center md:justify-between md:p-6 dark:border-gray-600">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                © 2023{" "}
+                <a href="https://flowbite.com/" className="hover:underline">
+                    Flowbite™
+                </a>
+                . All Rights Reserved.
+            </span>
+            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                    <a href="#" className="mr-4 hover:underline md:mr-6">
+                        About
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="mr-4 hover:underline md:mr-6">
+                        Privacy Policy
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="mr-4 hover:underline md:mr-6">
+                        Licensing
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </footer>
+
+    )
+}
