7b3cd34110981188ef1978708b5883d314f3131f
Jeffrey Desir <jeffrey@desir.tech>
2023-07-07 19:45:34 -0400

    dsdsd

--- www/src/views/includes/Footer/Footer.tsx
+++ www/src/views/includes/Footer/Footer.tsx
@@ -1,35 +1,52 @@
+"use client";
+
+import TextScramble from "@twistezo/react-text-scramble";
+
 export type FooterProps = {
-  copyright?: string;
-  tag?: string,
+  copyright?: string[];
+  tag?: string;
 };
 
-export const defaultProps = {
-  copyright: "© 2022 Kait Whitcomb",
-  tag: "Made w/ ❤️ by Desirable Solutions",
-  links: [
-    {
-      name: "Home",
-      url: "/",
-    }, {
-      name: "Home",
-      url: "/",
-    },
-    {
-      name: "Home",
-      url: "/",
-    },
-    {
-      name: "Home",
-      url: "/",
-    }
-  ]
-}
+
 
 export default function Footer({ copyright, tag }: FooterProps) {
+
+   const defaultProps = {
+    copyright: [
+      "Made w/ ❤️ by Desirable Solutions",
+      "678-323-2323",
+      "take a deep breath",
+      "therapy@drkaitwhitcomb.com",
+    ],
+    tag: "©️ Dr. Kait Whitcomb, 2023",
+    links: [
+      {
+        name: "Home",
+        url: "/",
+      },
+      {
+        name: "Info",
+        url: "/",
+      },
+      {
+        name: "About",
+        url: "/",
+      },
+      {
+        name: "Contact",
+        url: "/",
+      },
+    ],
+  };
   const Copyright = () => {
     return (
       <span className="m-2 text-black text-md sm:text-center">
-        {copyright ?? "© 2022 Kait Whitcomb"}
+        <TextScramble letterSpeed={1} texts={ [
+      "Made w/ ❤️ by Desirable Solutions",
+      "678-323-2323",
+      "take a deep breath",
+      "therapy@drkaitwhitcomb.com",
+    ]} />
       </span>
     );
   };
@@ -42,20 +59,19 @@ export default function Footer({ copyright, tag }: FooterProps) {
     );
   };
   const Links = () => {
-    const Link = (key: any, name: string) => {
+    const Link = ({key, name}: any) => {
       return (
         <li>
           <a href="#" className="mr-4 text-black hover:underline md:mr-6">
-           {`${name}`}
+            {`${name}`}
           </a>
         </li>
       );
     };
     return (
       <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-       {
-        defaultProps.links.map((link) => (
-          <Link {...link} key={Date.now()}/>
+        {defaultProps.links.map((link) => (
+          <Link {...link} key={Date.now()} />
         ))}
       </ul>
     );
@@ -65,8 +81,7 @@ export default function Footer({ copyright, tag }: FooterProps) {
     <footer className="w-full p-6 border-t border-black backdrop-blur-md relative-20 md:flex md:items-center md:justify-between md:p-6 dark:border-gray-600">
       <Tag />
       <Copyright />
-      <Links/>
- 
+      <Links />
     </footer>
   );
 }
