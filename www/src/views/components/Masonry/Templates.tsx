19b7b05c01133ede577c6477b5b6d67fc8d7b367
Jeffrey Desir <jeffrey@desir.tech>
2023-08-19 10:46:34 -0400

    rrr

--- www/src/views/components/Masonry/Templates.tsx
+++ www/src/views/components/Masonry/Templates.tsx
@@ -8,7 +8,7 @@ export function CalloutTemplate({ item }: any) {
   return (
     <Item sx="flex-start p-2" item={item}>
     <h1 className="w-full text-xl text-left text-white m-1 p-1 tracking-tight">{item.title}</h1>
-    <h2 className="text-lg text-left text-white opacity-90">{item.subTitle}</h2>
+    <h2 className="text-lg text-left text-white opacity-90 m-1 p-1">{item.subTitle}</h2>
     <blockquote className="text-lg text-left text-black p-1 m-1">{item.content}</blockquote>
     <a className="text-md text-left flex flex-row text-white m-1 p-1  border-white border-1" href={item?.link?.url ?? ""}>
       {item?.link?.label ?? ""}{!item?.link?.label ? <></>: <ArrowSVG/>} </a>
@@ -19,7 +19,7 @@ export function CalloutTemplate({ item }: any) {
 export function ContactTemplate({ item }: any) {
   return (
     <Item sx="items-center mb-2" item={item}>
-    <h1 className="px-6 py-2 text-2xl text-left text-white">{item.title}</h1>
+    <h1 className="px-6 py-2 text-xl text-left text-white">{item.title}</h1>
     <h2 className="px-4 text-lg text-left text-white opacity-90">{item.emailAddress}</h2>
     <blockquote className="text-xl text-left text-black flex flex-row">Phone {item.phoneNumber}</blockquote>
     <blockquote className="text-xl text-left text-black flex flex-row">Fax {item.faxNumber}</blockquote>
@@ -40,8 +40,8 @@ export function ImageTemplate({ item }: any) {
 export function DataTemplate({ item }: any) {
   return (
     <Item sx="flex-start" item={item}>
-      <h1 className="text-4xl">{item.title}</h1>
-      <blockquote>{item.content}</blockquote>
+      <h1 className="text-4xl p-1 m-1">{item.title}</h1>
+      <blockquote className="p-1 m-1">{item.content}</blockquote>
     </Item>
   )
 }
@@ -59,7 +59,7 @@ export function HeaderTemplate({ item }: any) {
 export function TextTemplate({ item }: any) {
   return (
     <Item item={item} >
-      <h2>{item.content}</h2>
+      <h2 className="m-1 p-1">{item.content}</h2>
     </Item>
   )
 }
